5a2902cc1fbee7ec47120805dc4f1269534ef64a
elinaviksne <elina@gmail.com>
2025-11-08 13:11:39 +0200

    3meģinanjums

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-const inputNumber = document.getElementById("text")
-const buttonPress = document.getElementById("button")
-const containerResult = document.getElementById("container")
+const inputNumber = document.getElementById("text");
+const buttonPress = document.getElementById("button");
+const containerResult = document.getElementById("container");
+const secondContainer = document.getElementById("secondContainer")
+buttonPress.addEventListener("click", click);
+function click(){
+    let R = inputNumber;
+    let PI = 3.14159;
+    const containerResult = PI*R*R;
+    const secondContainer = 2*PI*R
+}
